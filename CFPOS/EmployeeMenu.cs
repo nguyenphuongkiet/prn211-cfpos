59667ac1ba5d5a45ec2cf59331b5cb84152b2ff9
nguyenphuongkiet <KietNPSE172931@fpt.edu.vn>
2023-06-12 00:05:35 +0700

    fix combo box of item

--- CFPOS/EmployeeMenu.cs
+++ CFPOS/EmployeeMenu.cs
@@ -17,16 +17,14 @@ namespace CFPOS
     {
         CategoryRepository categoryRepository;
         ItemRepository itemRepository;
+        OrderDetailRepository orderDetailRepository;
+        OrderRepository orderRepository;
         public EmployeeMenu()
         {
             InitializeComponent();
-            categoryRepository = new CategoryRepository();
-            itemRepository = new ItemRepository();
-            cboCategory.DataSource = categoryRepository.getAll();
-            cboCategory.DisplayMember = "Name";
-            var selectedCategoryId = ((Category)cboCategory.SelectedItem).Id;
-            cboItem.DataSource = itemRepository.getAll().Where(a => a.CategoryId == selectedCategoryId).ToList();
-            cboItem.DisplayMember = "Name";
+
+            LoadItems();
+
         }
 
         private void btnTable1_Click(object sender, EventArgs e)
@@ -34,10 +32,56 @@ namespace CFPOS
             if (btnTable1.BackColor == Color.White)
                 btnTable1.BackColor = Color.Gray;
 
-            // Specify which columns to display
-            dgvOrder.Columns["name"].HeaderText = "Item Name";
-            dgvOrder.Columns["quantity"].HeaderText = "Quantity";
-            dgvOrder.Columns["price"].HeaderText = "Price";
+            
+        }
+
+
+        private void LoadItems()
+        {
+            // Query database to retrieve all available items
+            // Bind results to the ComboBox control
+            categoryRepository = new CategoryRepository();
+            itemRepository = new ItemRepository();
+            cboCategory.DataSource = categoryRepository.getAll();
+            cboCategory.DisplayMember = "Name";
+
+            // Add event handler for SelectedIndexChanged event
+            cboCategory.SelectedIndexChanged += CboCategory_SelectedIndexChanged;
+
+            var selectedCategoryId = ((Category)cboCategory.SelectedItem).Id;
+            cboItem.DataSource = itemRepository.getAll().Where(a => a.CategoryId == selectedCategoryId).ToList();
+            cboItem.DisplayMember = "Name";
+        }
+
+        private void CboCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedCategoryId = ((Category)cboCategory.SelectedItem).Id;
+            cboItem.DataSource = itemRepository.getAll().Where(a => a.CategoryId == selectedCategoryId).ToList();
+        }
+
+
+        private void LoadOrderDetails(int tableId)
+        {
+            orderDetailRepository = new OrderDetailRepository();
+            // Query database to retrieve order details for the specified table ID
+            // Bind the retrieved data to the DataGridView control
+            
+        }
+
+        private void AddItemButton_Click(object sender, EventArgs e)
+        {
+            // Get the selected item and quantity from the ComboBox and TextBox controls
+            // Add the item and quantity to the DataGridView control
+        }
+
+        private void PayButton_Click(object sender, EventArgs e)
+        {
+            // Create a new Order object
+            // Populate the Order object with necessary information (employee ID, table ID, total money, date, etc.)
+            // Insert the new Order into the database
+            // Iterate through the rows of the DataGridView control and insert each OrderDetail object into the database with the corresponding order ID
+            // Generate a bill or receipt based on the order information and print it out or export it as desired
+            // Reset the table selection and clear the DataGridView control to prepare for the next customer
         }
     }
 }
