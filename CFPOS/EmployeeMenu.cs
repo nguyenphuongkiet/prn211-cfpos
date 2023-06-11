2223eaa2475a17405a029d6fc62e6bdd7af7c802
nguyenphuongkiet <KietNPSE172931@fpt.edu.vn>
2023-06-11 23:44:33 +0700

    fix somthing + add combobox

--- CFPOS/EmployeeMenu.cs
+++ CFPOS/EmployeeMenu.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Services.Models;
+using Services.Repository;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,11 +15,29 @@ namespace CFPOS
 {
     public partial class EmployeeMenu : Form
     {
+        CategoryRepository categoryRepository;
+        ItemRepository itemRepository;
         public EmployeeMenu()
         {
             InitializeComponent();
+            categoryRepository = new CategoryRepository();
+            itemRepository = new ItemRepository();
+            cboCategory.DataSource = categoryRepository.getAll();
+            cboCategory.DisplayMember = "Name";
+            var selectedCategoryId = ((Category)cboCategory.SelectedItem).Id;
+            cboItem.DataSource = itemRepository.getAll().Where(a => a.CategoryId == selectedCategoryId).ToList();
+            cboItem.DisplayMember = "Name";
         }
 
+        private void btnTable1_Click(object sender, EventArgs e)
+        {
+            if (btnTable1.BackColor == Color.White)
+                btnTable1.BackColor = Color.Gray;
 
+            // Specify which columns to display
+            dgvOrder.Columns["name"].HeaderText = "Item Name";
+            dgvOrder.Columns["quantity"].HeaderText = "Quantity";
+            dgvOrder.Columns["price"].HeaderText = "Price";
+        }
     }
 }
