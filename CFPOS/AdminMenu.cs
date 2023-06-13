fafb480d97bf82a562ec7d94a6012d6ed032fdee
tanutv123 <taitanvo2309@gmail.com>
2023-06-13 11:36:59 +0700

    Update using user control form

--- CFPOS/AdminMenu.cs
+++ CFPOS/AdminMenu.cs
@@ -12,9 +12,34 @@ namespace CFPOS
 {
     public partial class AdminMenu : Form
     {
+
+        //static AdminMenu _obj;
+        //public static AdminMenu instance
+        //{
+        //    get
+        //    {
+        //        if (_obj == null)
+        //        {
+        //            _obj = new AdminMenu();
+        //        }
+        //        return _obj;
+        //    }
+        //}
+
+        //public Panel pnlContainer
+        //{
+        //    get { return panelContainer; }
+        //    set { panelContainer = value; }
+        //}
         public AdminMenu()
         {
             InitializeComponent();
+            
+        }
+
+        private void AdminMenu_Load(object sender, EventArgs e)
+        {
+            
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -22,6 +47,22 @@ namespace CFPOS
 
         }
 
+
+
+        private void btnSchedule_Click(object sender, EventArgs e)
+        {
+            if(!panelContainer.Controls.Contains(EmployeeSchedule.Instance))
+            {
+                panelContainer.Controls.Add(EmployeeSchedule.Instance);
+                EmployeeSchedule.Instance.Dock = DockStyle.Fill;
+                EmployeeSchedule.Instance.BringToFront();
+            }
+            else
+            {
+                EmployeeSchedule.Instance.BringToFront();
+            }
+
+        }
     }
-        
+
 }
